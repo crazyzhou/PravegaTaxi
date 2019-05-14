b64bf17d378b439633827fdc7decc3b2be4dc0f9
zhouy25 <brian.zhou@emc.com>
2019-05-14 16:20:11 +0800

    Implement Socket Writer

--- src/main/java/io/pravega/example/data/random/RandomNumberWriter.java
+++ src/main/java/io/pravega/example/data/random/RandomNumberWriter.java
@@ -1,7 +1,53 @@
 package io.pravega.example.data.random;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import io.pravega.client.ClientFactory;
+import io.pravega.client.admin.StreamManager;
+import io.pravega.client.stream.*;
+import io.pravega.client.stream.impl.JavaSerializer;
 
 public class RandomNumberWriter {
-    public static void main(String[] args) throws Exception{
+    private final String scope;
+    private final String streamName;
+    private final URI controllerURI;
 
+    public RandomNumberWriter(String scope, String streamName, URI controllerURI) {
+        this.scope = scope;
+        this.streamName = streamName;
+        this.controllerURI = controllerURI;
+    }
+
+    public void run(String routingKey) {
+        Random random = new Random();
+        StreamManager streamManager = StreamManager.create(controllerURI);
+        final boolean scopeIsNew = streamManager.createScope(scope);
+
+        StreamConfiguration streamConfig = StreamConfiguration.builder()
+                .scalingPolicy(ScalingPolicy.fixed(1))
+                .build();
+        final boolean streamIsNew = streamManager.createStream(scope, streamName, streamConfig);
+        try (
+            ClientFactory clientFactory = ClientFactory.withScope(scope, controllerURI);
+            EventStreamWriter<Double> writer = clientFactory.createEventWriter(streamName,
+                    new JavaSerializer<>(),
+                    EventWriterConfig.builder().build());) {
+            // Double in [0, 100)
+            writer.writeEvent(routingKey, random.nextDouble() * 100);
+        }
+    }
+
+    public static void main(String[] args) {
+        final String scope = "test-project";
+        final String streamName = "taxidata";
+        final URI controllerURI = URI.create("localhost:9090");
+        final String routingKey = "taxidata";
+        RandomNumberWriter writer = new RandomNumberWriter(scope, streamName, controllerURI);
+        writer.run(routingKey);
     }
 }
---
+++ src/main/java/io/pravega/example/data/socket/SocketWriter.java
@@ -0,0 +1,75 @@
+package io.pravega.example.data.socket;
+
+import io.pravega.client.ClientFactory;
+import io.pravega.client.admin.StreamManager;
+import io.pravega.client.stream.EventStreamWriter;
+import io.pravega.client.stream.EventWriterConfig;
+import io.pravega.client.stream.ScalingPolicy;
+import io.pravega.client.stream.StreamConfiguration;
+import io.pravega.client.stream.impl.JavaSerializer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URI;
+
+public class SocketWriter {
+    private final Socket socket;
+    private final String scope;
+    private final String streamName;
+    private final URI controllerURI;
+
+    public SocketWriter(Socket socket, String scope, String streamName, URI controllerURI) {
+        this.socket = socket;
+        this.scope = scope;
+        this.streamName = streamName;
+        this.controllerURI = controllerURI;
+    }
+
+    // listen to a socket
+    public void run(String routingKey) throws IOException {
+        String message;
+        StreamManager streamManager = StreamManager.create(controllerURI);
+        final boolean scopeIsNew = streamManager.createScope(scope);
+
+        StreamConfiguration streamConfig = StreamConfiguration.builder()
+                .scalingPolicy(ScalingPolicy.fixed(1))
+                .build();
+        final boolean streamIsNew = streamManager.createStream(scope, streamName, streamConfig);
+        try (
+                ClientFactory clientFactory = ClientFactory.withScope(scope, controllerURI);
+                EventStreamWriter<String> writer = clientFactory.createEventWriter(streamName,
+                        new JavaSerializer<>(),
+                        EventWriterConfig.builder().build());
+                InputStream is = socket.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            while (true) {
+                message = br.readLine();
+                if (message == null) continue;
+                if (message.equals("FINISH")) break;
+                writer.writeEvent(routingKey, message);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        final String scope = "test-project";
+        final String streamName = "socket";
+        final URI controllerURI = URI.create("localhost:9090");
+        final String routingKey = "socket";
+
+        // create a socket server
+        final ServerSocket ss = new ServerSocket(9999);
+        // wait and listen to the data input client.
+        Socket socket = ss.accept();
+        SocketWriter writer = new SocketWriter(socket, scope, streamName, controllerURI);
+
+        writer.run(routingKey);
+
+        socket.close();
+        ss.close();
+    }
+}
